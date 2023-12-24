702dd1e629bad06380cf275cef7442426e1e2304
kyle-233 <59004157+kyle-233@users.noreply.github.com>
2023-12-24 22:03:03 +0800

    Dev (#3)
    fix(style): login page and signup page input style fail

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,8 +2,8 @@ import { InputProps, Input as TInput} from 'tdesign-react'
 
 import styles from './Input.module.scss'
 
-export const Input = (props: InputProps) => {
+export const Input = ({className, ...props}: InputProps) => {
     return (
-        <TInput className={styles.input} {...props} />
+        <TInput className={`${styles.input} ${className ? className : ''}`} {...props} />
     )
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,10 @@
+import { useNavigate } from 'react-router-dom'
 import styles from './Login.module.scss'
 
 export const Login = () => {
+    const navigate = useNavigate()
     return (
-        <button type='button' className={styles.button}>
+        <button type='button' className={styles.button} onClick={() => {navigate('/login')}}>
             Log In
         </button>
     )
